5db7eab919562cc94c03299ee69d425ea83d3907
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-11-05 16:06:07 -0200

    Update Usuario.ts

--- Usuario.ts
+++ Usuario.ts
@@ -1,1 +1,22 @@
-# whatsApp
+import './style.css';
+import{Grupo} from "./Grupo";
+export class Usuario{
+  private nome : string;
+  private grupos : Array<Grupo>;
+  constructor(nome:string, grupos :Array<Grupo>){
+    this.nome = nome;
+    this.grupos = grupos;
+  }
+  public getNome(): string{
+    return this.nome;
+}public setNome(nome:string):void{
+    this.nome = nome;
+} public getGrupos(): Array<Grupo>{
+    return this.grupos;
+}public setGrupos(grupos: Array<Grupo>):void{
+    this.grupos = grupos;
+  }
+  public toString():string{
+    return this.nome;
+  }
+}
