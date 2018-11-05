292a7e610255db98860f86afd9a33965c4e5a7a7
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-11-05 16:06:47 -0200

    Update Menssagem.ts

--- Menssagem.ts
+++ Menssagem.ts
@@ -1,1 +1,8 @@
-# whatsApp
+import {Usuario} from "./Usuario";
+import {Grupo} from "./Grupo";
+
+export class Menssagem{
+  private texto: string;
+  private dono: Usuario;
+  private viu: Array<Usuario>;
+}
