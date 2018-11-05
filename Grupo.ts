c0d58a2cbf2190aae10ff3c2526441dd23c84170
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-11-05 16:05:41 -0200

    Update Grupo.ts

--- Grupo.ts
+++ Grupo.ts
@@ -1,1 +1,20 @@
-# whatsApp
+import{Usuario} from "./Usuario";
+import{Menssagem} from "./Menssagem";
+export class Grupo{
+  private nome : string;
+  private usuarios : Array<Usuario>;
+  private msg : Array<Menssagem>;
+  constructor(nome:string, usuarios : Array<Usuario>){
+    this.nome = nome;
+    this.usuarios = usuarios;
+  }
+  public getNome(): string{
+    return this.nome;
+}public setNome(nome:string):void{
+    this.nome = nome;
+} public getUsuarios(): Array<Usuario>{
+    return this.usuarios;
+}public setUsuarios(usuarios:Array<Usuario>):void{
+    this.usuarios = usuarios;
+  }
+}
