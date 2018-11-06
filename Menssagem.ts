59555a27decbc0999c411e0a1b5b99b714f3b15f
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-11-06 19:10:53 -0300

    Update Menssagem.ts

--- Menssagem.ts
+++ Menssagem.ts
@@ -5,4 +5,20 @@ export class Menssagem{
   private texto: string;
   private dono: Usuario;
   private viu: Array<Usuario>;
+  
+  public constructor(texto: string, dono: Usuario, viu: Usuario[]){
+    this.texto = texto;
+    this.dono = dono;
+    this.viu = viu;
+  }
+  
+  public getTexto():string{
+      return this.texto;
+  }public setTexto(texto:string): void{
+      this.texto = texto;
+  }public getDono():Usuario{
+      return this.dono;
+  }public setDono(dono:Usuario):void{
+      this.dono= dono;
+  }
 }
