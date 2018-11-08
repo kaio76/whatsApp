1be608c0c1f6761c3baf12bed92c049b8022e0da
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-11-07 22:29:36 -0200

    Update Grupo.ts

--- Grupo.ts
+++ Grupo.ts
@@ -4,7 +4,8 @@ export class Grupo{
   private nome : string;
   private usuarios : Array<Usuario>;
   private msg : Array<Menssagem>;
-  constructor(nome:string, usuarios : Array<Usuario>){
+
+  constructor(nome:string, usuarios : Array<Usuario>,msg : Array<Menssagem>){
     this.nome = nome;
     this.usuarios = usuarios;
   }
@@ -16,5 +17,12 @@ export class Grupo{
     return this.usuarios;
 }public setUsuarios(usuarios:Array<Usuario>):void{
     this.usuarios = usuarios;
+  } public getMsg(): Array<Menssagem>{
+    return this.msg;
+}public setMsg(msg:Array<Menssagem>):void{
+    this.msg =msg;
+  }
+  public toString():string{
+    return this.nome;
   }
 }
