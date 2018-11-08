1de16b962c924c47ff1396524bf770bcada7bd4a
kaio76 <42898648+kaio76@users.noreply.github.com>
2018-11-07 22:28:44 -0200

    Update Principal.ts

--- Principal.ts
+++ Principal.ts
@@ -1,20 +1,21 @@
 import {Usuario} from "./Usuario";
 import {Grupo} from "./Grupo";
 import {Menssagem} from "./Menssagem";
+import {Controlador} from "./Controlador";
 
 let usuarios : Arraylist<Usuario>=[];
 let grupos : Arraylist<Grupo>=[];
+let msg : Arraylist<Menssagem>=[];
 
 let opcao: string = readline.question("Escolha uma opção\n"+
 "addUser - Adicionar um Usuario\n"+
 "allUser - Mostar todos os Usuarios\n"+
 "newChat - Adicionar um Grupo\n"+
 "chats - Mostrar Grupo que um usuario pertence"+
-"allChat" - Mostrar todos os grupos"+                                      
+"allChat" - "Mostrar todos os grupos\n"+                        
 "invite - Convidar pessoas para o Grupo\n"+
 "leave - Deletar um usuario de um Grupo\n"+
 "zap - Mandar menssagem\n"+
-"notify - Mostar as notificações\n"+
 "ler - Ler as menssagens\n"+
 "users - Mostrar os usuarios de um Grupo");
 
@@ -33,11 +34,17 @@ switch(opcao){
   break;
   
   case "newChat":
-   let nom : string = readline.question(" Digite o nome");
+   let nom : string = readline.question("Digite o nome");
    let grupo : string = readline.question(" Digite o nome do Grupo");
-
-  let grup : Grupo = new Grupo(nom,grupo);
-    grupos.push(grupo);
+   if(this.testarUsuarios(usuarios:Usuario[])!= undefined){
+      console.log("Usuario invalido");
+   }else if(this.testarGrupos(grupos:Grupo[])!= undefined){
+      console.log("Grupo invalido");
+      {
+   }else{
+    let grup : Grupo = new Grupo(nom,grupo);
+    grupos.push(grup);
+    console.log("Grupo criado com sucesso")}
   break;
   case "allChat":
     for(let c of grupos){
@@ -46,26 +53,38 @@ switch(opcao){
     break;
 
   case "chats":
-  for(let i of usuarios){
-    console.log(i.toString, " ");
-  }
+ 
   break;
 
   case "invite":
-  for(let i of usuarios){
-    console.log(i.toString, " ");
-  }
+     addUserGrupo(res);
+     if(res == 0){
+       console.log("Inserido com sucesso");
+     }else if(res == 1){
+       console.log("Pessoa não existe");
+     }else if(res == 2){
+       console.log("grupo não existe");
+     }else if(res == 3){
+       console.log("Pessoa não existe no grupo");
+     }
   break;
 
   case "leave":
-  for(let i of usuarios){
-    console.log(i.toString, " ");
-  }
+ 
   break;
 
   case "zap":
-  for(let i of usuarios){
-    console.log(i.toString, " ");
-  }
+    enviarMenssagem(res);
+     if(res == 0){
+       console.log("Menssaem com sucesso");
+     }else if(res == 1){
+       console.log("Pessoa não existe");
+     }else if(res == 2){
+       console.log("grupo não existe");
+     }else if(res == 3){
+       console.log("Pessoa não existe no grupo");
+     }
+
   break;
   {
+
